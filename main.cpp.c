ffaa3e14d0a91ed82087ac699d06b122d38bf5a8
DjordjeMladenovicICT <142057305+DjordjeMladenovicICT@users.noreply.github.com>
2023-08-12 20:38:34 +0200

    Update main.cpp.c
    Računaj površinu i obim kruga

--- main.cpp.c
+++ main.cpp.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main()
 {
-float r, P;
+float r, P, O;
 printf("Uneti poluprecnik kruga:");
 scanf_s("%f", &r);
 P = r * r*3.14;
+O = 2 * r*3.14;  
 printf("Povrsina kruga je %.3f.", P);
+printf("Obim kruga je %.3f.", O);
 }
