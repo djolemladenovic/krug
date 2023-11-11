93528325f1f4f3aba58baa1e77c27aaaf0bdc8b6
profesorkamarina <150536998+profesorkamarina@users.noreply.github.com>
2023-11-11 20:23:33 +0100

    Update main.cpp.c

--- main.cpp.c
+++ main.cpp.c
@@ -2,10 +2,10 @@
 int main()
 {
 float r, P, O;
-printf("Uneti poluprecnik kruga:");
+printf("Uneti poluprecnik kruga r=/n");
 scanf_s("%f", &r);
 P = r * r*3.14;
 O = 2 * r*3.14;  
-printf("Povrsina kruga je %.3f.", P);
-printf("Obim kruga je %.3f.", O);
+printf("Povrsina kruga je %.2f.", P);
+printf("Obim kruga je %.2f.", O);
 }
