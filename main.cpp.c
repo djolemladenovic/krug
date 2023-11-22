4adb1ee90e6c44e881141c63bfa68789d1c92c40
Djordje Mladenović <127609209+djolemladenovic@users.noreply.github.com>
2023-11-22 22:47:02 +0100

    Update main.cpp.c
    Prosilili smo na tri decimale

--- main.cpp.c
+++ main.cpp.c
@@ -4,8 +4,8 @@ int main()
 float r, P, O;
 printf("Uneti poluprecnik kruga r=/n");
 scanf_s("%f", &r);
-P = r * r*3.14;
-O = 2 * r*3.14;  
+P = r * r*3.145;
+O = 2 * r*3.145;  
 printf("Povrsina kruga je %.3f.", P);
 printf("Obim kruga je %.3f.", O);
 }
